fccaf551bce115d6029a78c0b99a7338090d62a9
Vanessa Fiechtner <fiechtnervanessa@gmail.com>
2020-12-26 02:08:36 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -25,6 +25,14 @@ class Student
   end
 
   def self.drop_table
+    sql = <<-SQL 
+      DROP TABLE students 
+    SQL
+    DB[:conn].execute(sql)
+  end
+
+
+  def self.create(name, grade)  
     student = self.new(name, grade)
     student.save
     student
